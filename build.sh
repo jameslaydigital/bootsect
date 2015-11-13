da7966788a3b541295ee538321f36754749eb32f
James Lay Digital <jameslaymusic@gmail.com>
2015-11-12 20:25:31 -0700

    removed some unnecessary files

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 nasm -f bin -o stage1.bin stage_one.asm && \
 nasm -f bin -o stage2.bin stage_two.asm && \
 cat stage1.bin stage2.bin > raw.bin && \
-(mkdir cdiso || :) && \
-cp stage1.bin cdiso && cp stage2.bin cdiso && \
+#(mkdir cdiso || :) && \
+#cp stage1.bin cdiso && cp stage2.bin cdiso && \
 #mkisofs -o raw.iso -b stage1.bin cdiso/
 qemu-system-x86_64 raw.bin || \
 echo "COULD NOT FINISH ASSEMBLING." &>/dev/stderr
