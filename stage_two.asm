58b288602270a3e768cadd265fbcd628e6794839
James Lay Digital <jameslaymusic@gmail.com>
2015-11-12 20:32:00 -0700

    simplified stage_two.asm

--- stage_two.asm
+++ stage_two.asm
@@ -6,28 +6,23 @@ seg_two:
 %include "vga.asm"
 
 main:
-    call set_video_mode
-    push 226
-    call set_bg
-    pop ax
-    ;;push 5
-    ;;push 5
-    ;;push 5
-    ;;call set_letter_a
-    ;;pop ax
-    ;;pop ax
-    ;;pop ax
+    call set_video_mode ;set_video_mode()
 
-    ;; set_pxl ( color, x, y )
+    push 226
+    call set_bg         ;set_bg(color)
+    pop ax
+
     push 9
     push 9
     push 9
-    call set_pxl
-    add sp, 2
+    call set_pxl        ;set_pxl(color,x,y)
+    add sp, 2           ;rewinding sp by only 2 bytes because I
+                        ;will reuse the arguments from the stack
 
     push 8
-    call set_pxl
+    call set_pxl        ;set_pxl, changing only x
     add sp, 6
+
     jmp $
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
